560d8636f2234643bb7109e762481e83801f220f
anis <anis@farahanis>
2018-01-15 12:04:04 +0800

    commit 3.0

---
+++ java 2017/Utama/src/Tentangdiri.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * maklumat untuk memaparkan tentang diri
+ */
+public class Tentangdiri {
+    public static void main(String args[])     
+    {
+        System.out.println("Nama saya Ahmad");
+	System.out.println("Saya bersekolah di SMK Arus");
+	System.out.println("Saya berumur 16 tahun ");
+        System.out.println("Berat saya 50kg dan tinggi saya 178cm");
+	System.out.println("cita-cita saya ingin menjadi pengaturcara ");
+     }
+}
---
+++ java 2017/Utama/src/Utama.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anis
+ */
+public class Utama {
+    // public static void main(String args[])
+ public static void main(string[] args)
+  {       
+   system.out.println("Hello Worls");
+   system.out.println("Hello Worls");
+   system.out.println("Hello Worls");
+
+}
+}
